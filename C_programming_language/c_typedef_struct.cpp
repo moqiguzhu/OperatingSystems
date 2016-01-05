965943234dd05040996fbe39489694747df2145f
moqiguzhu <moqiguzhu@163.com>
2016-01-04 16:53:25 -0800

    remove some useless and redundant files

--- C_programming_language/c_typedef_struct.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-typedef struct Book
-{
-	char title[50];
-	char aythor[50];
-	char subject[100];
-	int book_id;
-} Book;
--- C_programming_language/test_open.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <fcntl.h>
-
-int main()
-{
-	int fd = open("foo", O_CREAT);
-	printf("%d\n", fd);
-}
